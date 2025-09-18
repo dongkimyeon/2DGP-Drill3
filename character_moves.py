6c2a28ddfbe0cce7c34aa4830449e58ddb4400fd
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-09-18 17:12:37 +0900

    캐릭터 원 운동 구현 완료

--- character_moves.py
+++ character_moves.py
@@ -10,9 +10,13 @@ def move_rectangle():
 
 def move_circle():
     print("Moving circle")
-    clear_canvas_now()
-    character.draw_now(400,300)
-    delay(0.1)
+    r = 200
+    for deg in range(0, 360):
+        x = r * math.cos(math.radians(deg)) + 400
+        y = r * math.sin(math.radians(deg)) + 300
+        clear_canvas_now()
+        character.draw_now(x, y)
+        delay(0.1)
     pass
 
 
