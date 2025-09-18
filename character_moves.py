64813189dce8ff5e982608c3389985ebc89e5b17
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-09-18 17:11:20 +0900

    캐릭터 출력 구현 완료

--- character_moves.py
+++ character_moves.py
@@ -3,12 +3,16 @@ from pico2d import *
 
 open_canvas()
 
+character = load_image('character.png')
 def move_rectangle():
     print("Moving rectangle")
     pass
 
 def move_circle():
     print("Moving circle")
+    clear_canvas_now()
+    character.draw_now(400,300)
+    delay(0.1)
     pass
 
 
